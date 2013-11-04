7792522e8a70c9c956500947ab0824035bca8a22
9miao Mobile Game <opensource@9miao.com>
2013-11-04 16:46:32 +0800

    Initial commit
    Signed-off-by: 9miao Mobile Game <opensource@9miao.com>

---
+++ firefly/firefly/firefly/management/__init__.py
@@ -0,0 +1,36 @@
+#coding:utf8
+'''
+Created on 2013-8-8
+
+@author: lan (www.9miao.com)
+'''
+from firefly.management import commands 
+import sys
+
+
+class Command:
+    
+    def __init__(self,subcommond,*args):
+        '''工具类指令
+        '''
+        self.subcommond = subcommond
+        self.args = args
+        
+    def execute(self):
+        '''
+        '''
+        commmd = getattr(commands,self.subcommond )
+        commmd.execute(*self.args)
+        
+        
+def execute_commands(*args):
+    '''
+    '''
+    if len(args)<2:
+        sys.stdout.write("command error")
+    subcommand = args[1]
+    comm = Command(subcommand,*tuple(args[2:]))
+    comm.execute()
+    
+    
+    
